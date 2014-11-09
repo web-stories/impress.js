242afd9629f1147ecc625a9b355353ba9e1577e5
Fagner Brack <github@fagnermartins.com>
2014-11-09 07:15:12 -0200

    Add grunt
    Closes gh-1. Closes gh-2.

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+module.exports = function( grunt ) {
+	"use strict";
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON( "package.json" ),
+		jscs: {
+			files: [
+				"Gruntfile.js",
+				"src/**/*.js"
+			]
+		},
+		jshint: {
+			files: [
+				"Gruntfile.js",
+				"src/**/*.js"
+			],
+			options: {
+				"node": true
+			}
+		},
+		concat: {
+			options: {
+				banner: grunt.file.read( "src/banner/banner.js" ),
+				footer: grunt.file.read( "src/banner/footer.js" )
+			},
+			core: {
+				files: {
+					"dist/<%= pkg.name %>-<%= pkg.version %>.js": [ "src/js/core.js" ]
+				}
+			},
+			all: {
+				files: {
+					"dist/<%= pkg.name %>.all-<%= pkg.version %>.js": [
+						"src/js/core.js",
+						"src/js/navigation.js"
+					]
+				}
+			}
+		}
+	});
+
+	// Load grunt tasks from NPM packages
+	require( "load-grunt-tasks" )( grunt );
+
+	// Main tasks
+	grunt.registerTask( "files", [ "concat" ] );
+	grunt.registerTask( "validate", [ "jscs", "jshint" ] );
+
+	// Special tasks
+	grunt.registerTask( "default", [ "validate", "files" ] );
+};
