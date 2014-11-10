44bb6746841d8df370eb1d25001dc8178b20bfa9
Fagner Brack <github@fagnermartins.com>
2014-11-10 06:02:44 -0200

    Navigation code review
    Closes gh-5.

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -4,9 +4,9 @@
 // It's because these navigation actions only need what impress.js provides with
 // its simple API.
 
-// throttling functioncalls, by Remy Sharp
+// Debounce function calls, by Remy Sharp.
 // http://remysharp.com/2010/07/21/throttling-function-calls/
-var throttle = function( fn, delay ) {
+var debounce = function( fn, delay ) {
 	var timer = null;
 	return function() {
 		var context = this, args = arguments;
@@ -17,26 +17,45 @@ var throttle = function( fn, delay ) {
 	};
 };
 
-// wait for impress.js to be initialized
+// Enumerate all keyCodes as constants to enhance readability.
+var keyCodes = {
+	ARROW_DOWN: 40,
+	ARROW_LEFT: 37,
+	ARROW_RIGHT: 39,
+	ARROW_UP: 38,
+
+	PAGE_DOWN: 34,
+	PAGE_UP: 33,
+
+	SPACE: 32,
+	TAB: 9
+};
+
+// Wait for impress.js to be initialized.
 document.addEventListener( "impress:init", function( event ) {
 	// Getting API from event data.
-	// So you don't event need to know what is the id of the root element
-	// or anything. `impress:init` event data gives you everything you
-	// need to control the presentation that was just initialized.
+	// So you don't event need to know what is the id of the root element or anything.
+	// `impress:init` event data gives you everything you need to control the presentation that was
+	// just initialized.
 	var api = event.detail.api;
 
 	// KEYBOARD NAVIGATION HANDLERS
 
 	// Prevent default keydown action when one of supported key is pressed.
 	document.addEventListener( "keydown", function( event ) {
-		if (
-			event.keyCode === 9 ||
-			( event.keyCode >= 32 && event.keyCode <= 34 ) ||
-			( event.keyCode >= 37 && event.keyCode <= 40 )
-		) {
-			event.preventDefault();
+		switch ( event.keyCode ) {
+			case keyCodes.ARROW_DOWN:
+			case keyCodes.ARROW_LEFT:
+			case keyCodes.ARROW_RIGHT:
+			case keyCodes.ARROW_UP:
+			case keyCodes.PAGE_DOWN:
+			case keyCodes.PAGE_UP:
+			case keyCodes.SPACE:
+			case keyCodes.TAB:
+				event.preventDefault();
+				break;
 		}
-	}, false );
+	});
 
 	// Trigger impress action (next or prev) on keyup.
 
@@ -45,55 +64,50 @@ document.addEventListener( "impress:init", function( event ) {
 	// [up] [right] / [down] [left] - again common and natural addition,
 	// [pgdown] / [pgup] - often triggered by remote controllers,
 	// [tab] - this one is quite controversial, but the reason it ended up on
-	//   this list is quite an interesting story... Remember that strange part
-	//   in the impress.js code where window is scrolled to 0,0 on every presentation
-	//   step, because sometimes browser scrolls viewport because of the focused element?
-	//   Well, the [tab] key by default navigates around focusable elements, so clicking
-	//   it very often caused scrolling to focused element and breaking impress.js
-	//   positioning. I didn't want to just prevent this default action, so I used [tab]
-	//   as another way to moving to next step...
+	//  this list is quite an interesting story... Remember that strange part
+	//  in the impress.js code where window is scrolled to 0,0 on every presentation
+	//  step, because sometimes browser scrolls viewport because of the focused element?
+	//  Well, the [tab] key by default navigates around focusable elements, so clicking
+	//  it very often caused scrolling to focused element and breaking impress.js
+	//  positioning. I didn't want to just prevent this default action, so I used [tab]
+	//  as another way to moving to next step...
 	document.addEventListener( "keyup", function( event ) {
-		if (
-			event.keyCode === 9 ||
-			( event.keyCode >= 32 && event.keyCode <= 34 ) ||
-			( event.keyCode >= 37 && event.keyCode <= 40 )
-		) {
-			switch ( event.keyCode ) {
-				case 33: // pg up
-				case 37: // left
-				case 38: // up
-						api.prev();
-						break;
-				case 9: // tab
-					api[ event.shiftKey ? "prev" : "next" ]();
-					break;
-				case 32: // space
-				case 34: // pg down
-				case 39: // right
-				case 40: // down
-						api.next();
-						break;
-			}
-
-			event.preventDefault();
+		switch ( event.keyCode ) {
+			case keyCodes.PAGE_UP:
+			case keyCodes.ARROW_LEFT:
+			case keyCodes.ARROW_UP:
+				api.prev();
+				event.preventDefault();
+				break;
+			case keyCodes.TAB:
+				api[ event.shiftKey ? "prev" : "next" ]();
+				event.preventDefault();
+				break;
+			case keyCodes.SPACE:
+			case keyCodes.PAGE_DOWN:
+			case keyCodes.ARROW_RIGHT:
+			case keyCodes.ARROW_DOWN:
+				api.next();
+				event.preventDefault();
+				break;
 		}
 	}, false );
 
-	// delegated handler for clicking on the links to presentation steps
+	// Delegated handler for clicking on the links to presentation steps.
 	document.addEventListener( "click", function( event ) {
-		// event delegation with "bubbling"
-		// check if event target (or any of its parents is a link)
+		var href;
 		var target = event.target;
-		while ( ( target.tagName !== "A" ) &&
-				( target !== document.documentElement ) ) {
+
+		// Event delegation with "bubbling" check if event target (or any of its parents) is a link.
+		while ( target.nodeName !== "A" && target !== document.documentElement ) {
 			target = target.parentNode;
 		}
 
-		if ( target.tagName === "A" ) {
-			var href = target.getAttribute( "href" );
+		if ( target.nodeName === "A" ) {
+			href = target.getAttribute( "href" );
 
-			// if it's a link to presentation step, target this step
-			if ( href && href[ 0 ] === "#" ) {
+			// If it's a link to presentation step, target this step.
+			if ( href && href.charAt( 0 ) === "#" ) {
 				target = document.getElementById( href.slice( 1 ) );
 			}
 		}
@@ -102,26 +116,29 @@ document.addEventListener( "impress:init", function( event ) {
 			event.stopImmediatePropagation();
 			event.preventDefault();
 		}
-	}, false );
+	});
 
-	// delegated handler for clicking on step elements
+	// Delegated handler for clicking on step elements.
 	document.addEventListener( "click", function( event ) {
 		var target = event.target;
-		// find closest step element that is not active
-		while (
-			!( target.classList.contains( "step" ) &&
-				!target.classList.contains( "active" ) ) &&
-			( target !== document.documentElement ) ) {
+		var bluredStep = function() {
+			var step = target.classList.contains( "step" );
+			var active = target.classList.contains( "active" );
+			return step && !active;
+		};
+
+		// Find other closest step element that is not active.
+		while ( !bluredStep() && target !== document.documentElement ) {
 			target = target.parentNode;
 		}
 
 		if ( api.goto( target ) ) {
 			event.preventDefault();
 		}
-	}, false );
+	});
 
-	// touch handler to detect taps on the left and right side of the screen
-	// based on awesome work of @hakimel: https://github.com/hakimel/reveal.js
+	// Touch handler to detect taps on the left and right side of the screen, based on awesome work
+	// of @hakimel: https://github.com/hakimel/reveal.js
 	document.addEventListener( "touchstart", function( event ) {
 		if ( event.touches.length === 1 ) {
 			var x = event.touches[ 0 ].clientX;
@@ -138,12 +155,14 @@ document.addEventListener( "impress:init", function( event ) {
 				event.preventDefault();
 			}
 		}
-	}, false );
+	});
 
-	// rescale presentation when window is resized
-	window.addEventListener( "resize", throttle(function() {
-		// force going to active step again, to trigger rescaling
-		api.goto( document.querySelector( ".step.active" ), 500 );
-	}, 250 ), false );
+	// Rescale presentation when window is resized.
+	// Force going to active step again, to trigger rescaling.
+	window.addEventListener( "resize", debounce(function() {
+		var stepElement = document.querySelector( ".step.active" );
+		var duration = 500;
+		api.goto( stepElement, duration );
+	}, 250 ));
 
 }, false );
